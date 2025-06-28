713e2c137bdde13abff63a50f4e74ecada3a3d2d
miltiaboy <114646328+miltiaboy@users.noreply.github.com>
2025-06-28 11:37:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -115,8 +115,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(  
             video="https://envs.sh/2TO.mp4",          
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),  
-            reply_markup = reply_markup,
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),            
             parse_mode=enums.ParseMode.HTML
         )
         return
